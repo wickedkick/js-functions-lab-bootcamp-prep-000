292274b9db256dd0ad13095e00290546d06c6a08
Imran Iqbal <imran1337@gmail.com>
2019-08-06 22:31:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return(`Happy Holidays, ${name}!`)
+  return(`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo(holiday, name) {
